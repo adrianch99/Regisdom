aa321368c2dea0d78132182ba9598de84706a641
adrianch99 <charrisadrianjose@gmail.com>
2025-12-05 17:03:03 -0500

    ....

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,13 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-    user: 'postgres', 
-    host: 'localhost',
-    database: 'regisdom', 
-    password: 'mondongo',
-    port: 5432,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    port: process.env.PGPORT,
+    ssl: { rejectUnauthorized: false } // Asegura la conexión SSL
 });
 
 module.exports = pool;
